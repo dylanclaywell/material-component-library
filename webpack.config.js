ecd896a98e0213942fb8e7c60b881cda09f659b3
Dylan Claywell <djclaywell@gmail.com>
2021-02-12 23:35:28 -0600

    Added a way to view the components in a dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
-let entry = {
-  react: {
-    import: path.resolve(__dirname, './src/App.tsx'),
-  },
+const plugins = [new CleanWebpackPlugin()]
+
+if (isDev) {
+  plugins.push(
+    new HtmlPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+    })
+  )
 }
+
+const entry = isDev
+  ? path.resolve(__dirname, './src/App.tsx')
+  : path.resolve(__dirname, './src/index.ts')
+const libraryTarget = isDev ? undefined : 'commonjs'
 
 module.exports = {
   devtool: 'source-map',
   devServer: {
-    contentBase: 'dist/public',
+    contentBase: 'dist',
   },
-  entry: path.resolve(__dirname, './src/index.ts'),
+  entry,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs',
+    libraryTarget,
   },
   target: 'web',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins,
   module: {
     rules: [
       {
